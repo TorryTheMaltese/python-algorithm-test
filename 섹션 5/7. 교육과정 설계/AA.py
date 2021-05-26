83be61d2ad9ddb1323fe82ac410b7820c1ab9530
TorryTheMaltese <torryiscute@gmail.com>
2021-05-26 23:59:40 +0900

    try section 5 q7

--- 섹션 5/7. 교육과정 설계/AA.py
+++ 섹션 5/7. 교육과정 설계/AA.py
@@ -1,28 +1,43 @@
 import sys
-from collections import deque
-#sys.stdin=open("input.txt", "r")
-need=input()
-n=int(input())
-for i in range(n):
-    plan=input()
-    dq=deque(need)
-    for x in plan:
-        if x in dq:
-            if x!=dq.popleft():
-                print("#%d NO" %(i+1))
-                break
-    else:
-        if len(dq)==0:
-            print("#%d YES" %(i+1))
+
+sys.stdin = open("in1.txt", "rt")
+
+required_subject = list(input())
+n = int(input())
+class_plan = list()
+
+for _ in range(n):
+    class_plan.append(list(input()))
+
+
+def check_class_plan(class_list):
+    print("check ", class_list)
+
+    tmp = required_subject
+    print('require ',tmp)
+    while len(tmp) > 0:
+        print(tmp)
+        print(class_list)
+        first = class_list[0]
+        # print(first, class_list[0])
+        if len(class_list) > 0:
+            if tmp[0] == first:
+                tmp.pop(0)
+                class_list.pop(0)
+            else:
+                class_list.pop(0)
         else:
-            print("#%d NO" %(i+1))
+            return False
+    return True
 
 
+for i in range(n):
+    if check_class_plan(class_plan[i]):
+        print('#', i + 1, ' YES')
+    else:
+        print('#', i + 1, ' NO')
 
+print(check_class_plan())
 
-
-
-
-
-
-
+# print(required_subject)
+# print(class_plan)
