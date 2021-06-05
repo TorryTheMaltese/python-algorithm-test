8f7c2848810400adf19a28e6c4c99773a9d46105
TorryTheMaltese <torryiscute@gmail.com>
2021-06-05 09:03:11 +0900

    section 6 q4 solved

--- 섹션 6/4. 합이 같은 부분집합/AA.py
+++ 섹션 6/4. 합이 같은 부분집합/AA.py
@@ -1,21 +1,33 @@
 import sys
-#sys.stdin=open("input.txt", "r")
-def DFS(L, sum):
-    if sum>total//2:
-        return
-    if L==n:
-        if sum==(total-sum):
+
+# sys.stdin = open("in4.txt", "rt")
+
+
+def dfs(v):
+    if v > n:
+        return False
+    else:
+        sum0 = 0
+        sum1 = 0
+        for i in range(1, n + 1):
+            if check[i] == 0:
+                sum0 += arr[i]
+            if check[i] == 1:
+                sum1 += arr[i]
+        if (sum0 != 0 and sum1 != 0) and sum0 == sum1:
             print("YES")
             sys.exit(0)
-    else:
-        DFS(L+1, sum+a[L])
-        DFS(L+1, sum)
 
-if __name__=="__main__":
-    n=int(input())
-    a=list(map(int, input().split()))
-    total=sum(a)
-    DFS(0, 0)
-    print("NO")
+        check[v] = 1
+        dfs(v + 1)
+        check[v] = 0
+        dfs(v + 1)
 
-    
+
+if __name__ == "__main__":
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.insert(0, 0)
+    check = [0] * (n + 1)
+    if not dfs(1):
+        print("NO")
